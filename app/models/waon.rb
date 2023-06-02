ce61df9d4640b57526c0db9d7363a87687580fd3
sabanonitsuke <sabakan.nari@gmail.com>
2023-06-03 00:13:14 +0900

    waonモデルの削除

--- app/models/waon.rb
+++
@@ -1,2 +0,0 @@
-class Waon < ApplicationRecord
-end
--- db/migrate/20230602145751_create_waons.rb
+++
@@ -1,8 +0,0 @@
-class CreateWaons < ActiveRecord::Migration[6.0]
-  def change
-    create_table :waons do |t|
-
-      t.timestamps
-    end
-  end
-end
